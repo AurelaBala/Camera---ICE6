7116422c6c8311dc41dcb24ee0aac28305ab6d50
AurelaBala <aurelatosuni2022@gmail.com>
2023-03-12 19:22:12 -0400

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Aurela Bala on 2023-03-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
